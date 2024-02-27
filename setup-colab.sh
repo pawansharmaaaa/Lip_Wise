6b6d08247874cc6a28a48ba186c123a744cc9aaa
pawansharmaaaa <18pawansharma72000@gmail.com>
2024-02-28 03:53:49 +0530

    modified:   setup-colab.sh

--- setup-colab.sh
+++ setup-colab.sh
@@ -5,19 +5,15 @@ then
     sudo apt-get install ffmpeg
 fi
 
-# Check if CUDA is installed
-if [ -z "$CUDA_PATH" ]
-then
-    echo "CUDA is not installed. Please install CUDA."
-    echo "CPU will be used for inference."
-fi
-
 # Install requirements
 pip install -r requirements.txt
 pip install --upgrade --no-cache-dir gdown
 
+# Find python version
+python_version=$(python -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")
+
 # Copy archs
-cp archs/* /usr/local/lib/python3.10/dist-packages/basicsr/archs
+cp archs/* /usr/local/lib/python${python_version}/dist-packages/basicsr/archs
 
 # Run file_check.py
 python ./helpers/file_check.py
